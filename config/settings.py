3794ef149ecc51f47493bc37ded7799e97caddcb
Vicky91Ch <vicky.joha@gmail.com>
2026-06-27 16:40:07 -0500

    Actualizacion de settings.py

--- config/settings.py
+++ config/settings.py
@@ -21,19 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+wvvtnn8k77qn$e_kjqqq)&ta@7q(!!hgck^y2r#&ap&sk+0^4'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-    "20.172.64.205",
-    "chicaiza-motos.uaeftt-ute.site",
-]
-
-
+SECRET_KEY    = config('SECRET_KEY')
+DEBUG         = config('DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 # Application definition
 
 INSTALLED_APPS = [
